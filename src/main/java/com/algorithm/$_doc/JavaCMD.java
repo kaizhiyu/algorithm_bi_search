74b7dd763824be2f38e9665ddf45fc067ab96ca0
sleevan <522400740@qq.com>
2017-12-24 00:35:35 +0800

    java命令

---
+++ src/main/java/com/algorithm/$_doc/JavaCMD.java
@@ -0,0 +1,28 @@
+package com.algorithm.$_doc;
+
+import com.algorithm.$8_annotation.Doc4Desc;
+
+/**
+ * @author fanhb on 2017/12/24
+ * @desc xxxx
+ */
+
+@Doc4Desc("java命令")
+public class JavaCMD {
+
+    /**
+     *
+     * 1: 打包jar文件
+     *      jar cvf  main.jar Test4Java.class
+     * 2: 解压jar文件
+     *      jar xf main.jar
+     *
+     * 3: 更新Main-Class: Test4Java.class
+     *      jar    umf    MANIFEST.MF    app.jar 　(应该是hello.jar吧)　
+     * 4: exec
+     *      java jar main.jar
+     *
+     * https://www.cnblogs.com/mq0036/p/3885399.html
+     *
+     */
+}
