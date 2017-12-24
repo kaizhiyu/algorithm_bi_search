323fe417b2ba918f67cb2811660c87176a84d4e7
sleevan <522400740@qq.com>
2017-12-24 22:09:01 +0800

    https://github.com/TheAlgorithms/Java.git

---
+++ src/main/java/com/algorithm/$1_bi/search/BinarySearchDoc.java
@@ -0,0 +1,14 @@
+package com.algorithm.$1_bi.search;
+
+import com.algorithm.$8_annotation.Doc4Desc;
+
+/**
+ * @author fanhb on 2017/12/24
+ * @desc xxxx
+ */
+
+@Doc4Desc("this is only doc for algorithm")
+public class BinarySearchDoc {
+
+    //https://github.com/TheAlgorithms/Java.git
+}
