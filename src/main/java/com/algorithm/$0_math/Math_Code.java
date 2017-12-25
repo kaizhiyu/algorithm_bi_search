fd8a8bd26bed846b33c0ca3b8ddfe60280c1af9e
fanhaibo <522400740@qq.com>
2017-12-25 15:54:55 +0800

    math as code

--- src/main/java/com/algorithm/$0_math/Math_Code.java
+++ src/main/java/com/algorithm/$0_math/Math_Code.java
@@ -12,5 +12,26 @@ import com.algorithm.$8_annotation.Doc4Desc;
 @Doc4Desc("数学方面的代码")
 public class Math_Code {
 
+    /**
+     * math-as-code
+
+     译注：译者英文与数学水平都非常有限，尝试翻译，如有错误请指正。英文原版 在此。
+     这是一份通过对比数学符号和JavaScript代码来帮助开发者更容易了解数学符号的参考。
+
+     动机:学术论文可能会吓着自学游戏和图形的程序猿:)
+
+     这个指南还没有完成。如果你发现错误或者想要贡献，请open a ticket或发一个 PR。
+
+     注意： 简洁起见，有些代码示例使用了npm 包。你可以到他们的GitHub repos来查看实现的详细情况。
+     前言
+
+     数学符号可以表示不同的意思，这取决于作者，上下文和所学习的领域（线性代数，集合理论，等等）。
+     这份指南也许不会涵盖符号的所有用法。在某些情况，会引用一些真       实材料（博客文章,出版物等等）来演示某个符号的实际用法。
+
+     更完整的列表，请看Wikipedia - List of Mathematical Symbols。
+
+     简单起见，这里许多的代码示例都操作浮点数值，并不是数字健壮的（numerically robust）。
+     为什么这会是一个问题的更多细节请看Robust Arithmetic Notes 作者是 Mikola Lysenko。
+     */
     // https://github.com/Jam3/math-as-code/blob/master/README-zh.md
 }
