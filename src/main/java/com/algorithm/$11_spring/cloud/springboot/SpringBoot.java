8cabd0e06c5e27eee5fc18d57de83249c17bdf26
fanhaibo <522400740@qq.com>
2017-12-26 19:37:47 +0800

    Initializing Spring FrameworkServlet 'dispatcherServlet'
    处理mvc延迟加载的问题

---
+++ src/main/java/com/algorithm/$11_spring/cloud/springboot/SpringBoot.java
@@ -0,0 +1,11 @@
+package com.algorithm.$11_spring.cloud.springboot;
+
+/**
+ * xxx
+ *
+ * @author:v_fanhaibo on 2017/12/26.
+ * @version:v1.0
+ */
+
+public class SpringBoot {
+}
---
+++ src/main/java/com/algorithm/$11_spring/cloud/springboot/springboot_doc.java
@@ -0,0 +1,2 @@
+//mvc.servlet.load-on-startup=-1
+//处理启动延迟的问题  Initializing Spring FrameworkServlet 'dispatcherServlet'
