d12906530e29e70eff209836e3b2a0f3c8c3fbca
fanhaibo <522400740@qq.com>
2017-12-27 18:20:27 +0800

    添加日子工具类

--- src/main/java/com/algorithm/$12_logutils/LogUtils.java
+++ src/main/java/com/algorithm/$12_logutils/LogUtils.java
@@ -3,7 +3,7 @@ package com.algorithm.$12_logutils;
 import com.algorithm.$5_json.JsonMapper;
 
 /**
- * xxx
+ * 日志工具类
  *
  * @author:v_fanhaibo on 2017/12/27.
  * @version:v1.0
@@ -11,8 +11,10 @@ import com.algorithm.$5_json.JsonMapper;
 
 public class LogUtils {
 
-    /** record param by using Jackson */
-    public static void infoUseJson(Object targetClass, Object messageToJson,String... otherMessageToJson) {
+    /**
+     * record param by using Jackson
+     */
+    public static void infoUseJson(Object targetClass, Object messageToJson, String... otherMessageToJson) {
 
         org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(targetClass.getClass());
         if (log.isInfoEnabled()) {
@@ -20,7 +22,7 @@ public class LogUtils {
             StringBuilder sb = new StringBuilder(messageToJson.getClass().getSimpleName());
             sb.append(" ;request json: ").append(strInfo);
 //            sb.append(" ;curUserNo: ").append(UserContext.getCurrentuserNo().orElse("no userNo "));
-            if (otherMessageToJson != null && otherMessageToJson.length != 0){
+            if (otherMessageToJson != null && otherMessageToJson.length != 0) {
                 sb.append(" ;other message: ").append(JsonMapper.toJson(otherMessageToJson));
             }
             log.info(sb.toString());
