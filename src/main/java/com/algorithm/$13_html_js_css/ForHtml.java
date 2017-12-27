ce629017981038490e7e5474f27ef22347d4286a
fanhaibo <522400740@qq.com>
2017-12-27 21:00:29 +0800

    js

---
+++ src/main/java/com/algorithm/$13_html_js_css/ForHtml.java
@@ -0,0 +1,11 @@
+package com.algorithm.$13_html_js_css;
+
+/**
+ * xxx
+ *
+ * @author:v_fanhaibo on 2017/12/27.
+ * @version:v1.0
+ */
+
+public class ForHtml {
+}
---
+++ src/main/java/com/algorithm/$13_html_js_css/Testjs.js
@@ -0,0 +1,10 @@
+console.info("hello!!");
+var person1 = {id: 1, "firstname": "John", "lastname": "Doe", "age": 50, "eyecolor": "blue"};
+var person2 = {id: 2, "firstname": "John", "lastname": "Doe", "age": 50, "eyecolor": "blue"};
+
+var arr1 = [];
+var arr2 = [];
+
+person1.id.is
+ // console.info(arr1.contains(person1.id))
+
