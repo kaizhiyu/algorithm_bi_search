2c806f40d5d2894b4cee8cd0f5f0e9ce9f6007a8
fanhaibo <522400740@qq.com>
2017-12-29 17:21:13 +0800

    添加fastjson

--- src/main/java/com/algorithm/$2_java8/User.java
+++ src/main/java/com/algorithm/$2_java8/User.java
@@ -1,5 +1,13 @@
 package com.algorithm.$2_java8;
 
+import com.algorithm.$5_json.JsonMapper;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * xxx
  *
@@ -7,22 +15,30 @@ package com.algorithm.$2_java8;
  * @version:v1.0
  */
 
-public class User {
+public class User implements Serializable{
 
-    private Byte code;
+    private Integer code;
 
-    public Byte getCode() {
+    public Integer getCode() {
         return code;
     }
 
-    public void setCode(Byte code) {
+    public void setCode(Integer code) {
         this.code = code;
     }
 
     public static void main(String[] args) {
-        User user = new User();
-//        user.setCode(new Byte(1));
-        Byte.valueOf(1);
-        Byte aByte = new Byte((byte) 1);
+//        User user = new User();
+//        user.setCode(1);
+//
+//        System.out.println(JsonMapper.toJson(user));
+//
+        String json = "{\"code\":\"0001\"}";
+//        System.out.println(JsonMapper.getObjFromJson(json, new TypeReference<User>() {
+//        }));
+        User user = JSON.parseObject(json, new com.alibaba.fastjson.TypeReference<User>() {});
+        System.out.println(JSON.toJSON(user));
+
+
     }
 }
