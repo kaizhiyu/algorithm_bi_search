6e94f9d542fd383ca27b2654d8d06ddb7253813a
sleevan <522400740@qq.com>
2018-01-02 20:24:57 +0800

    queue

---
+++ src/main/java/com/algorithm/$3_concurrent/pactice/fifteen/chapter/LinkedQueue.java
@@ -0,0 +1,51 @@
+package com.algorithm.$3_concurrent.pactice.fifteen.chapter;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @author fanhb on 2018/1/1
+ * @desc xxxx
+ */
+public class LinkedQueue<E> {
+
+
+    private static class Node<E> {
+        final E item;
+        final AtomicReference<Node<E>> nextNode;
+
+        public Node(E item, Node<E> nextNode) {
+            this.item = item;
+            this.nextNode = new AtomicReference<>(nextNode);
+        }
+    }
+
+    private Node<E> dummy = new Node<>(null, null);
+    private AtomicReference<Node<E>> head = new AtomicReference<>(dummy);
+    private AtomicReference<Node<E>> tail = new AtomicReference<>(dummy);
+
+    public boolean put(E item) {
+
+        Node<E> newNode = new Node<>(item, null);
+
+        while (true) {
+
+            Node<E> curTail = tail.get();
+            Node<E> nextTail = curTail.nextNode.get();
+            if (curTail == tail.get()) {
+                if (nextTail != null) {
+                    tail.compareAndSet(curTail, nextTail);
+                } else {
+                    if (curTail.nextNode.compareAndSet(null,newNode)){
+                        tail.compareAndSet(curTail,newNode);
+                        return true;
+                    }
+                }
+
+
+            }
+        }
+
+
+    }
+
+}
