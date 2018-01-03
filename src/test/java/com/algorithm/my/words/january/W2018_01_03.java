7bdeb294cfa2b0d39fce47b1299fbbafdb0af7ae
fanhaibo <522400740@qq.com>
2018-01-03 16:52:46 +0800

    处理角色重复的异常，targetRole

--- src/test/java/com/algorithm/my/words/january/W2018_01_03.java
+++ src/test/java/com/algorithm/my/words/january/W2018_01_03.java
@@ -1,12 +1,16 @@
 package com.algorithm.my.words.january;
 
+import com.algorithm.$8_annotation.Doc4Desc;
+
 /**
  * xxx
  *
  * @author:v_fanhaibo on 2018/1/3.
  * @version:v1.0
+ *
  */
 
+@Doc4Desc("《java并发编程实践》 第二章，线程安全性")
 public class W2018_01_03 {
 
     //english:
@@ -17,15 +21,15 @@ public class W2018_01_03 {
     //05: servlet,JavaServer Page(JSP)
     //06: marshaled:                                    列集
     //07: unmarshaled                                   散集
-    //08:
-    //09:
+    //08: scrap                                         ，n，碎片；adj,废弃的；
+    //09: mutable                                       ,adj,可变的；
     //10:
     //11:
 
 
     /** 中文:
-     * 01: 线程调度器：操作系统的核心，实际是一个常驻内存的程序，不断地对线程队列进行扫描，
-     * 02:
+     * 01: 线程调度器：                                   ，操作系统的核心，实际是一个常驻内存的程序，不断地对线程队列进行扫描，
+     * 02: 对象状态：                                     ，任何影响其外部可见行为的数据；
      * 03:
      * 04:
      * 05:
