533b44aa8bda76133244701f774e45e219195d0e
fanhaibo <522400740@qq.com>
2018-01-03 15:50:06 +0800

    添加word

--- src/test/java/com/algorithm/my/words/W01_January.java
+++ src/test/java/com/algorithm/my/words/W01_January.java
@@ -1,3 +1,2 @@
 package com.algorithm.my.words;
 
-//dash: 猛冲，v
--- src/test/java/com/algorithm/my/words/W2018_01_03.java
+++ src/test/java/com/algorithm/my/words/W2018_01_03.java
@@ -8,4 +8,34 @@ package com.algorithm.my.words;
  */
 
 public class W2018_01_03 {
+
+    //english:
+    //01: dash: 猛冲，v
+    //02: curial: 关键的，The red cog is curial
+    //03: immutable: adj,不可变的；
+    //04: RMI: remote method invocation,远程方法调用
+    //05: servlet,JavaServer Page(JSP)
+    //06:
+    //07:
+    //08:
+    //09:
+    //10:
+    //11:
+
+
+    /** 中文:
+     * 01: 线程调度器：操作系统的核心，实际是一个常驻内存的程序，不断地对线程队列进行扫描，
+     * 02
+     * 03
+     * 04
+     * 05
+     * 06
+     * 07
+     * 08
+     * 09
+     * 10
+     * 11
+     *
+     *
+     */
 }
