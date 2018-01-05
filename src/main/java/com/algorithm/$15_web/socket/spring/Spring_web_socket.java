7295d9a81f3a994c02d5f8768a0bc20233e0d213
fanhaibo <522400740@qq.com>
2018-01-05 13:24:58 +0800

    经过验证，这个是发布与订阅消息

--- src/main/java/com/algorithm/$15_web/socket/spring/Spring_web_socket.java
+++ src/main/java/com/algorithm/$15_web/socket/spring/Spring_web_socket.java
@@ -5,6 +5,9 @@ package com.algorithm.$15_web.socket.spring;
  * //https://www.jianshu.com/p/60799f1356c5
  * //web socket 跨域了 Origin header value not allowed. Even though URL is allowed
  * http://blog.csdn.net/tengxing007/article/details/77949746
+ *
+ *
+ * 经过验证，这个是发布与订阅消息
  */
 
 public class Spring_web_socket {
