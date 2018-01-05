8192168491bed90ed1147e2d7a3f743e6e0fde02
fanhaibo <522400740@qq.com>
2018-01-05 14:27:15 +0800

    sass 安装命令

---
+++ src/main/java/com/algorithm/$13_html_js_css/npm/Npm_sass_install.java
@@ -0,0 +1,22 @@
+package com.algorithm.$13_html_js_css.npm;
+
+/**
+ * npm install sass -g
+ *
+ * @author:v_fanhaibo on 2018/1/5.
+ * @version:v1.0
+ */
+
+public class Npm_sass_install {
+
+    /**
+     * sass 安装命令
+     *
+     * npm install sass -g
+     *
+     * js执行命令
+     * node xxx.js
+     *
+     *
+     */
+}
