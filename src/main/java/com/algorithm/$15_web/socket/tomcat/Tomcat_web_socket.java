fe2ebccb1b33d3035aa19f88350ac07be2430fc7
fanhaibo <522400740@qq.com>
2018-01-05 09:57:56 +0800

    添加两种websocket——demo

--- src/main/java/com/algorithm/$15_web/socket/tomcat/Tomcat_web_socket.java
+++ src/main/java/com/algorithm/$15_web/socket/tomcat/Tomcat_web_socket.java
@@ -1,11 +1,6 @@
 package com.algorithm.$15_web.socket.tomcat;
 
 /**
- * xxx
- *
- * @author:v_fanhaibo on 2018/1/4.
- * @version:v1.0
- *
  * webSocketDemo
  * //https://www.jianshu.com/p/60799f1356c5
  *
@@ -13,5 +8,5 @@ package com.algorithm.$15_web.socket.tomcat;
  * http://blog.csdn.net/tengxing007/article/details/77949746
  */
 
-public class Spring_web_socket {
+public class Tomcat_web_socket {
 }
