5e61e0cead01cb512f8304d21acc9da2c9dd9dbf
fanhaibo <522400740@qq.com>
2018-01-10 17:26:30 +0800

    job cmd

---
+++ src/main/java/com/algorithm/$_java_tools/$5_job/cmd/JobCmd.java
@@ -0,0 +1,17 @@
+package com.algorithm.$_java_tools.$5_job.cmd;
+
+import com.algorithm.$8_annotation.Doc4Desc;
+
+/**
+ * JOB CMD
+ *
+ * @author:v_fanhaibo on 2018/1/10.
+ * @version:v1.0
+ */
+@Doc4Desc("credit-*.py 项目名称 kill")
+@Doc4Desc("credit-*.py 项目名称 start")
+@Doc4Desc("/opt/app")
+public class JobCmd {
+
+
+}
