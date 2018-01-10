e5eba49177fd48d6b265131fa25f2ba5feee59eb
fanhaibo <522400740@qq.com>
2018-01-10 17:55:58 +0800

    昨日文件

---
+++ src/test/java/com/test/ytd/$2018_01_10_cef.java
@@ -0,0 +1,20 @@
+package com.test.ytd;
+
+import com.algorithm.$8_annotation.Doc4Desc;
+
+/**
+ * @author:v_fanhaibo on 2018/1/10.
+ * @version:v1.0
+ */
+
+@Doc4Desc("昨日文件")
+@Doc4Desc("Java_cmd_cef_desc")
+public class $2018_01_10_cef {
+
+    //specify application entry point
+    // for stand-alone application
+    // bundled into an executable jar file
+    //指定捆绑到可执行jar文件中的独立应用程序的应用程序入口点
+
+
+}
