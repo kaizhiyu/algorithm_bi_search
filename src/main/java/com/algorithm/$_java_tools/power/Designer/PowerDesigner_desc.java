e1f7a1f321b1de8b021ca85aded243887dddcaf6
fanhaibo <522400740@qq.com>
2018-01-10 16:58:43 +0800

    powerdesigner 显示中文名称

---
+++ src/main/java/com/algorithm/$_java_tools/power/Designer/PowerDesigner_desc.java
@@ -0,0 +1,13 @@
+package com.algorithm.$_java_tools.power.Designer;
+
+import com.algorithm.$8_annotation.Doc4Desc;
+
+/**
+ *  //http://blog.csdn.net/shandalue/article/details/39031563
+ *  powerDesigner显示中文注释字段
+ * @author:v_fanhaibo on 2018/1/10.
+ * @version:v1.0
+ */
+@Doc4Desc("power designer 显示中文名字")
+public class PowerDesigner_desc {
+}
