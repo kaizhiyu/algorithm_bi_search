b2afd1d4bbba913cf3b24ee46140a2c0072e618c
fanhaibo <522400740@qq.com>
2018-01-11 11:26:13 +0800

    update

--- src/main/java/com/algorithm/$19_todo/list/TodoFirstEveryday.java
+++ src/main/java/com/algorithm/$19_todo/list/TodoFirstEveryday.java
@@ -1,11 +1,12 @@
 package com.algorithm.$19_todo.list;
 
+import com.algorithm.$8_annotation.single.ann.SearchKeyWord;
 import com.algorithm.$8_annotation.single.ann.TODOList;
 
 /** @auth v_fanhaibo on   2018/1/11. */
+@SearchKeyWord("Daily mission Daily tasks")
 
 
-@TODOList("Daily mission Daily tasks")
 @TODOList("1: algorithm")
 @TODOList("2: 2 kinds of words such as  en, zh  ")
 @TODOList("3: anything of ytd   ytd yesterday   ")
