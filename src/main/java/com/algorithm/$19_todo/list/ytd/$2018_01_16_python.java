19d0b3032fb9d596dbe6c568484018605ba02d73
fanhaibo <522400740@qq.com>
2018-01-16 17:46:35 +0800

    python idea cc

--- src/main/java/com/algorithm/$19_todo/list/ytd/$2018_01_16_python.java
+++ src/main/java/com/algorithm/$19_todo/list/ytd/$2018_01_16_python.java
@@ -11,11 +11,27 @@ import com.algorithm.$8_annotation.single.ann.SearchKeyWord;
 public class $2018_01_16_python {
 
     /**
+     * 男儿何不带吴钩，收取关山五十州，请君暂上凌烟阁，若个书生万户侯。
+     * 窃钩者诛窃国者侯；
+     */
+
+
+    /**
      *
      * 1: https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_3120347246249872985%22%7D&n_type=0&p_from=1
      * 恋爱八年不分你我？领完证后老公拿出七张卡……
      * 2: 停格,侧田,红色高跟鞋,蔡健雅
-     * 3:
+     * 3: Python基础教程，Page23
+     * 4：Page25:第一章小结；
+     * 5：第二章：Page26, 序列：列表和元祖（字符串是什么序列，不能修改）
+     *                          列表和元祖的区别是，列表可以修改，元祖不能修改
+     *            2.25 成员资格 2018年1月16日14:20:01
+     *
+     * 6：granted: 准许
+     * 7: extend	英[ɪkˈstend] 美[ɪkˈstɛnd] vt.	延伸; 扩大; 推广;
+     * 8: 2.5： 序列小结：列表和元祖，元祖
+     * 9: ---Page45
+     *
      *
      */
 
--- src/main/java/com/algorithm/$_java_tools_cmd/$tc8_idea/For_idea.java
+++ src/main/java/com/algorithm/$_java_tools_cmd/$tc8_idea/For_idea.java
@@ -9,10 +9,10 @@ import java.util.List;
  */
 public class For_idea {
     //1:    live Template  ctrl + shift + A: searchKey Live Template
-        //PIC
-        //    /** $var1$ */
-        //    private String $var2$;
-        //    $end$
+    //PIC
+    //    /** $var1$ */
+    //    private String $var2$;
+    //    $end$
     //2:    edit -> find ->Select All Occurrences  //Occurrences: 发生的事
 
     //3: 100.fori
@@ -26,7 +26,6 @@ public class For_idea {
     //10: show diagram: 基础结构 == cammand + H
     //11: call hierarchy :method调用
     //12: 光标: http://picawen.leanote.com/post/IntelliJ-IDEA快捷键
-
 
     private static void sss(String name) {
         System.out.printf("name:%s%n", name);
