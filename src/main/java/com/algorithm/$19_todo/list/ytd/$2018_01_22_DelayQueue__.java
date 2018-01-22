da60231df4fa24be9b406a6627520fc5a37ed65a
sleevan <522400740@qq.com>
2018-01-22 21:43:30 +0800

    dq

---
+++ src/main/java/com/algorithm/$19_todo/list/ytd/$2018_01_22_DelayQueue__.java
@@ -0,0 +1,15 @@
+package com.algorithm.$19_todo.list.ytd;
+
+import com.algorithm.$8_annotation.single.ann.SearchKeyWord;
+import com.algorithm.$8_annotation.single.ann.TODOList;
+
+/**
+ * @author:v_fanhaibo on 2018/1/11
+ * @version:v1.0
+ */
+@SearchKeyWord("delayQueue 延时队列")
+@TODOList("dq")
+public class $2018_01_22_DelayQueue__ {
+    //TODO
+
+}
