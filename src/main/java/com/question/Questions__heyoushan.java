6fa297ce3adfda6664c437a57a4eb3d7c840a358
sleeve <522400740@qq.com>
2018-02-09 15:32:57 +0800

    good question

---
+++ src/main/java/com/question/Questions__heyoushan.java
@@ -0,0 +1,7 @@
+package com.question;
+
+/**
+ * @auth v_fanhaibo on   2018/2/9
+ */
+public class Questions__heyoushan {
+}
---
+++ src/main/java/com/question/q1_demo/q_question_little_demo.java
@@ -0,0 +1,9 @@
+package com.question.q1_demo;
+
+/**
+ * @auth v_fanhaibo on   2018/2/9
+ */
+public class q_question_little_demo {
+
+    //最重要的是先提出一个好的问题
+}
