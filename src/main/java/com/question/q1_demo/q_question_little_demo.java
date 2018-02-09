a7b897d52488022fc84beecce394d94b46d17d6c
sleeve <522400740@qq.com>
2018-02-09 17:17:37 +0800

    good question

--- src/main/java/com/question/q1_demo/q_question_little_demo.java
+++ src/main/java/com/question/q1_demo/q_question_little_demo.java
@@ -5,5 +5,12 @@ package com.question.q1_demo;
  */
 public class q_question_little_demo {
 
+    /** ① good question */
+    /** ② good question */
+    /** ③ good question */
+    /** ④ good question */
+    /** ⑤ good question */
+    /** ⑥ good question */
+
     //最重要的是先提出一个好的问题
 }
