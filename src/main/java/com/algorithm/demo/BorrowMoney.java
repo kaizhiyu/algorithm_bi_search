c2daa7c1ed4b7d6341dec75f7859b17f45099320
sleeve <522400740@qq.com>
2018-03-05 16:58:01 +0800

    borrow money

---
+++ src/main/java/com/algorithm/demo/BorrowMoney.java
@@ -0,0 +1,27 @@
+package com.algorithm.demo;
+
+/**
+ * @auth v_fanhaibo on   2018/3/5
+ */
+public class BorrowMoney {
+
+    /**
+     * zqn          +1
+     * supan        +0.5        -0.5
+     * hj           +0.5        -1
+     * jt           +2          -2
+     * xsj          +1
+     * xrh          + 0.4       -0.6
+     *
+     * -----------------------
+     * fgl          +3
+     * ttx          +2
+     * ljy          +1
+     *
+     * WenBo        +2
+     * ym           +1
+     *
+     * fm           +3
+     * zj           15
+     */
+}
