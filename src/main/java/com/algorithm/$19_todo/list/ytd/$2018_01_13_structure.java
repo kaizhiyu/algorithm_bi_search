c5f528f24d96287c1837bc00c0884415cf5c7722
sleevan <522400740@qq.com>
2018-03-07 20:51:01 +0800

    insertSort

--- src/main/java/com/algorithm/$19_todo/list/ytd/$2018_01_13_structure.java
+++ src/main/java/com/algorithm/$19_todo/list/ytd/$2018_01_13_structure.java
@@ -2,6 +2,8 @@ package com.algorithm.$19_todo.list.ytd;
 
 import com.algorithm.$8_annotation.single.ann.SearchKeyWord;
 
+import java.util.Arrays;
+
 /**
  * @author:v_fanhaibo on 2018/1/11
  * @version:v1.0
@@ -12,12 +14,36 @@ public class $2018_01_13_structure {
 
     /**
      * 1: modules:  These courses cover a twelve-week period and are organised into three four-week modules
-     *
+     * <p>
      * 2: 格局,眼界  structure
-     *
-     *
-     *
      */
+
+
+    public static void main(String[] args) {
+        int[] a = {5, 2, 7, 9, 60, 11, 33, 55, 35, 67, 76, 45, 34, 12};
+        insertSort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+
+    /**
+     * 插入排序,采用移位,而不是交换;
+     *
+     * @param a
+     */
+    public static void insertSort(int[] a) {
+        int j;
+        for (int p = 0; p < a.length; p++) {
+            int tmp = a[p];
+            for (j = p; j > 0 && tmp < a[j - 1]; j--) {
+                a[j] = a[j - 1];
+            }
+            a[j] = tmp;
+
+        }
+
+    }
+
 
 
 
