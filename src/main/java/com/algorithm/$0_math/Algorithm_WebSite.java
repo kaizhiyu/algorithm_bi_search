532c02b305fb818cbfda24ae425f06baa5a2e50e
sleevan <522400740@qq.com>
2018-04-12 12:46:13 +0800

    十大排序算法

---
+++ src/main/java/com/algorithm/$0_math/Algorithm_WebSite.java
@@ -0,0 +1,17 @@
+package com.algorithm.$0_math;
+
+
+import com.algorithm.$8_annotation.single.ann.Location;
+
+@Location("排序算法 ___ 快速排序 归并排序 ")
+public class Algorithm_WebSite {
+
+
+    /**
+     *
+     * http://img.blog.csdn.net/20160917110641479
+     *
+     *
+     *
+     */
+}
---
+++ src/main/java/com/algorithm/$1_bi/search/access/ArrayList_VS_Linked.java
@@ -0,0 +1,19 @@
+package com.algorithm.$1_bi.search.access;
+
+
+import com.algorithm.$8_annotation.single.ann.Location;
+
+@Location("数组 和 链表 ")
+public class ArrayList_VS_Linked {
+
+    /**
+     * https://blog.csdn.net/islandww/article/details/72511737
+     *
+     *
+     * CPU 寄存器 – immediate access (0-1个CPU时钟周期)
+     * CPU L1 缓存  – fast access (3个CPU时钟周期)
+     * CPU L2 缓存 – slightly slower access (10个CPU时钟周期)
+     * 内存 (RAM)   – slow access (100个CPU时钟周期)
+     * 硬盘 (file system) – very slow (10,000,000个CPU时钟周期)
+     */
+}
