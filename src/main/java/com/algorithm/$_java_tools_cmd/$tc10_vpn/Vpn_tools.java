3baf7f7b7a4ba554048c4637a5e1c7999d47e608
sleevan <522400740@qq.com>
2018-04-15 23:24:25 +0800

    add vpn

---
+++ src/main/java/com/algorithm/$_java_tools_cmd/$tc10_vpn/Vpn_tools.java
@@ -0,0 +1,18 @@
+package com.algorithm.$_java_tools_cmd.$tc10_vpn;
+
+import com.algorithm.$8_annotation.single.ann.SearchKeyWord;
+
+/**
+ * @auth v_fanhaibo on   2018/1/31
+ */
+
+@SearchKeyWord("vpn tools shadowsocks")
+public class Vpn_tools {
+
+    /**
+     * https://github.com/shadowsocks/ShadowsocksX-NG/releases
+     * 
+     */
+
+
+}
