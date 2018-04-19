8b7f5d296e8c56a80c1f70e853f86ae83fac914c
sleevan <522400740@qq.com>
2018-04-19 09:47:14 +0800

    http://en-yinbiao.xiao84.com/biao/
    英语发音

---
+++ src/main/java/com/algorithm/$_doc/Englist_voice_.java
@@ -0,0 +1,15 @@
+package com.algorithm.$_doc;
+
+import com.algorithm.$8_annotation.single.ann.Location;
+
+@Location("英文发音  english ")
+public class Englist_voice_ {
+
+    /**
+     * http://en-yinbiao.xiao84.com/yinbiaofayin/e-sound.html
+     *
+     * http://en-yinbiao.xiao84.com/biao/
+     *
+     *
+     */
+}
